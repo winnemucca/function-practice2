ecee1a6d72011e19dea6ea0f462b687bc302e066
Steve Kauyedauty <skauyedauty@gmail.com>
2014-08-06 14:12:25 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,126 @@
+// var getName=function(person){
+
+// 	return person.name;
+
+
+// }
+// console.log(getName({ name: 'Luisa', age: 25 }));
+// console.log(getName({ name: 'ron', age: 12 }));
+
+
+// 	var totalLetters=function(words){
+// 		var count =0;
+// 		for (var i =0; i<words.length; i++){
+// 			count+=words[i].length;
+// 		}
+// 			return count;
+// 	}
+// 	console.log(totalLetters(['javascript', 'is', 'awesome']));
+
+
+
+// 	var keyValue = function(city){
+// 		var object={
+// 			city: city,
+// 		};
+// 		return object;
+// 	};
+// 	console.log(keyValue('Denver'));
+
+
+
+	// var negativeIndex =function(array, negIndex){
+		
+	// 	// find the array langth
+	// 	var arr= array.length + negIndex;
+
+	// 	return array[arr];
+
+
+
+	// }
+	// console.log("negative index", negativeIndex(['a', 'b', 'c', 'd', 'e'], -2));
+
+	
+
+// var removeM = function(string){
+
+// 	//find the index of a string
+// 	var myString= string.split('');
+// // 	return count;
+// 	for(i=0 ; i<myString.length; i++){
+
+// 		if (myString[i]=== "m"){
+//  			myString.splice(i, 1);
+
+// 		}
+		
+// 	}
+// 	var combine= myString.join('');
+// console.log(combine);
+// }
+// removeM('family');
+// removeM('memory');
+
+
+
+
+
+// var printObject=function(set){
+// 	// a: 10,
+// 	// b: 20,
+// 	// c: 30
+
+	
+// 	for(key in set){
+		 
+
+// console.log(key, " is ", set[key]);
+// 	}
+
+// };
+// printObject({ a: 10, b: 20, c: 30 });
+
+
+// var vowels= function(string){
+// newArray= [];
+
+// var vowelList= ['a', 'e', 'i', 'o', 'u'];
+
+
+// 	// if((string === letterA) || (string ===letterE) || (string === letterI) || (string === letterU) || (string === letterO)){
+// 		for(var i =0; i<string.length; i++){
+
+
+// 			if(vowelList.indexOf(string[i]) !== -1){
+				
+
+// 				newArray.push(string[i]);
+		
+
+// 			}
+// 		}
+// 		console.log(newArray);
+// };
+// vowels('alabama');
+// vowels('oxford');
+
+
+var twins = function(){
+
+
+};
+twins(['a', 'a', 'b', 'b', 'c', 'c']);
+
+
+
+
+
+
+
+
+
+
+
+
+
